e7c65127395fa1c09ee4fc628b954957cfd9be92
Ravshanbek Tursunbaev <rtursunbaev9822@gmail.com>
2023-01-05 13:48:37 +0500

    updating calculator logic struct with comments

--- Calculator/CalculatorLogic.swift
+++ Calculator/CalculatorLogic.swift
@@ -10,21 +10,26 @@ import Foundation
 struct CalculatorLogic {
     
    private var number:Double?
+    //variable to hold first entered number and operation
    private var intermediateOperation:(n1:Double, operation:Int)?
     
+    //method to set number variable
    mutating func setNumber(_ number:Double){
         self.number = number
     }
     
+//calculate the result according to the passed sybmol
   mutating  func calcMethod(_ symbol:Int?)->Double?{
+      //if symbol and number exists
         if let symbol, let number{
-            if symbol == 2{
+            
+            if symbol == 2{       // symbol 2 is a negative or position number maker
                return number * -1
-            }else if symbol == 3{
+            }else if symbol == 3{ // symbol 3 is a changer to percentage
                return number * 0.01
-            }else if symbol == 1{
+            }else if symbol == 1{ // symbol 1 is reset button
                 return 0
-            }else if symbol == 8{
+            }else if symbol == 8{ //symbol 8 is a equal button
                return calcResult()
             }else{
                 self.intermediateOperation = (n1:number, operation:symbol)
@@ -34,8 +39,8 @@ struct CalculatorLogic {
       return nil
     }
     
+    // if equal button pressed, calulate final result method
     private func calcResult()->Double?{
-        
         if let n1 = intermediateOperation?.n1, let operation = intermediateOperation?.operation, let n2 = number{
             switch operation{
             case 7:
